3c23791d19aa4db73c8f28d5ea8ac7f11a039dd5
WillysKapitalink <wilson.castro@kpitalink.com>
2020-11-12 18:28:39 -0500

    update

--- src/test/java/com/uisek/servicio/inscripcion/ServicioAgenciasApplicationTests.java
+++ src/test/java/com/uisek/servicio/inscripcion/ServicioAgenciasApplicationTests.java
@@ -1,4 +1,4 @@
-package com.cempresariales.servicio.agencias;
+package com.uisek.servicio.inscripcion;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
